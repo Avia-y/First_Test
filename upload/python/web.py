3e3d6abdc725fc1173e0b167055ab6072949f923
xieyanrimu <68262014+xieyanrimu@users.noreply.github.com>
2021-07-07 16:39:16 +0800

    Update web.py
    使用读取excel表的方式获取url

--- upload/python/web.py
+++ upload/python/web.py
@@ -1,5 +1,6 @@
 import os
 
+import xlrd
 from selenium import webdriver
 import time
 from PIL import Image
@@ -39,21 +40,22 @@ def open_url(test_url, num):
     browser.get(test_url)
     time.sleep(15)
     browser.save_screenshot('E:\web\c\c%d.png' % num)
-    # 关闭当前窗口，并定位到第一个窗口
     browser.close()
     browser.switch_to.window(handles[0])
     return
 
 
-# 打开浏览器
+# 开始计时
+time_start = time.time()
+
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get('https://cloud.chaojidun.com/home')
-# browser.get('https://xxxx.com')
 
 # 定位到要右击的元素
 double_click = browser.find_element_by_link_text('登录')
 double_click.click()
+time.sleep(5)
 
 # 登录
 zh = browser.find_element_by_class_name("el-input__inner")
@@ -73,38 +75,60 @@ time.sleep(5)
 # cf = browser.find_element_by_xpath("//*[@id=\"app\"]/div/div[2]/div[1]/div/ul[1]")
 # cf.click()
 
-n = 0
-
 # url = "https://ecs-buy4service.aliyun.com/wizard/#/prepay/cn-beijing"
 # open_url(url)
 
-# file是url列表，file2用于记录结果
-file = open(r"E:\web\url.txt", "r", encoding="utf-8")
+# file = open(r"E:\web\test.txt", "r", encoding="utf-8")
+# file = open(r"E:\web\url.txt", "r", encoding="utf-8")
+# f = file.readlines()
+# n = 1
+
+# file2用于记录结果
 file2 = open(r"E:\web\res.txt", "w+", encoding="utf-8")
 
-f = file.readlines()
+# 打开excel
+excel = xlrd.open_workbook(r"E:\web\url.xlsx")
+# 选择表一
+sht = excel.sheets()[0]
+
 # 遍历url列表
-for url in f:
+# for url in f:
+for n in range(1, sht.nrows):
+    url = sht.cell(n, 3).value
+
     print(url)
     open_url(url, n)
+
     # 对比网页图片是否异常
     fp1 = "E:\web\c\c" + str(n) + ".png"
     fp2 = "E:\web\log\c" + str(n) + ".png"
     res = pil_image_similarity(fp1, fp2)
     print(str(res))
-    # 保存差值大于5000的图片
+    # 当差异值大于5000时，保存截图并将结果保存到res.txt中
     if res > 5000:
-        file2.write(str(n) + ':' + url)
-        file2.write(str(res) + '\n')
+        file2.write(str(n + 1) + ': ' + url + '\n')
+        file2.write(str(res) + '\n\n')
         file2.flush()
+    elif url == 'https://www.aliyun.com/?e=1101':
+        os.remove(fp1)
     else:
         os.remove(fp1)
-    n += 1
+    # n += 1
 
-# 关闭file文件
-file.close()
+# file.close()
+
 file2.close()
 
 # 关闭浏览器
 time.sleep(5)
 browser.quit()
+
+# 计时结束
+time_end = time.time()
+t = time_end - time_start
+m = t / 60
+s = t - 60 * int(m)
+ms = s - int(s)
+# print(t, ',', int(m), ',', round(s, 2))
+print('花费总时间：', int(m), 'm', int(s), 's', int(round(ms, 2) * 100))
+
